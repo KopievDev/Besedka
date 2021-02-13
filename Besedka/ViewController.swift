7030fd8c37725b13ffdd5039e692fe172ed9543e
Kopiev <x@mustdev.ru>
2021-02-13 21:10:19 +0300

    Git init

---
+++ Besedka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Besedka
+//
+//  Created by Ivan Kopiev on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
