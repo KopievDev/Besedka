66f04581a2f536244ac9ea5a9e628cd0d62eb636
Kopiev <x@mustdev.ru>
2021-02-13 23:50:29 +0300

    Add Application Lifecycle tracking

--- Besedka/AppDelegate.swift
+++ Besedka/AppDelegate.swift
@@ -11,14 +11,57 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var state: String = "" //Состояние приложения
+    
+    //Сообщает делегату, что процесс запуска начался
+    func application(_ application: UIApplication,
+                     willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool{
+        state = #function
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-      
+        print("The start of the method \(#function)")
         return true
     }
+    
+    //Сообщает делегату, что процесс запуска почти завершен и приложение почти готово к запуску.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
 
+        return true
+    }
+    
+    //Сообщает делегату, что приложение стало активным.
+    func applicationDidBecomeActive(_ application: UIApplication) {
 
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
+        
+      }
+    
+    //Сообщает делегату, что приложение вот-вот станет неактивным.
+    func applicationWillResignActive(_ application: UIApplication) {
+
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
+    }
+    
+    //Сообщает делегату, что приложение теперь находится в фоновом режиме.
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
+    }
+    //Сообщает делегату, что приложение вот-вот выйдет на передний план.
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
+    }
+    //Сообщает делегату, когда приложение собирается завершить работу.
+    func applicationWillTerminate(_ application: UIApplication) {
+
+        print("Application moved from \(state) to \(#function):  \(#function) ")
+        state = #function
+    }
 }
-
--- Besedka/ViewController.swift
+++ Besedka/ViewController.swift
@@ -9,58 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stateViewController: String = "" // Состояние ViewController
     
+    
+    //MARK: - LoadView
+    //Этот метод вызывается при создании View
     override func loadView() {
         super.loadView()
-        print(#function)
-        }
-
+        
+        self.stateViewController = #function
+        print("The start of the method \(#function)")
+        
+    }
+    
+    //MARK: - LoadDidView
+    //Этот метод загружается один раз в жизненном цикле ViewControllera .Он вызывается, когда загружаются все View
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function)
 
-        
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
     }
+    
+    //MARK: - viewWillAppear
+    //Этот метод вызывается каждый раз перед тем, как вьюшка будет видна и перед тем, как будет настроена какая-либо анимация
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print(#function)
-
+        
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
     }
+    
+    //MARK: - viewDidAppear
+    //Этот метод вызывается после загрузки View на экране.
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print(#function)
 
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
     }
     
+    //MARK: - viewWillLayoutSubviews
+    //Этот метод ничего не делает по умолчанию. Когда границы вида изменяются, он корректирует положение своих подвидов.
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        print(#function)
 
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print(#function)
-
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print(#function)
-
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print(#function)
-
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
         
     }
     
+    //MARK: - viewDidLayoutSubviews
+    //Этот метод вызывается после того, как viewController был приспособлен к своему подвиду после изменения его привязки
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
+        
+    }
     
+    //MARK: - viewWillDisappear
+    //Этот метод вызывается до того, как вьюшка будет удалена из иерархии вьюшек.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
+        
+    }
+    
+    //MARK: - viewDidDisappear
+    //Этот метод вызывается после того, как вьюшка будет удалена из иерархии вьюшек.
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
 
+        print("ViewController moved from \(stateViewController) to \(#function):  \(#function) ")
+        self.stateViewController = #function
+        
+        
+    }
     
 }
 
