2ec04028a90d7709af6cddb8202ed05868a25f4e
Kopiev <x@mustdev.ru>
2021-02-25 18:20:11 +0300

    Fix Bugs v1,1

--- Besedka/ProfileVC/ProfileViewController.swift
+++ Besedka/ProfileVC/ProfileViewController.swift
@@ -15,7 +15,9 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var shortName: UILabel!
-    
+    @IBOutlet weak var nameBetweenDesc: NSLayoutConstraint!
+    @IBOutlet weak var nameBetweenImage: NSLayoutConstraint!
+    @IBOutlet weak var imageBetweenTop: NSLayoutConstraint!
     
     //MARK: - IBAction
     @IBAction func editButtonPressed(_ sender: Any) {
@@ -80,19 +82,19 @@ class ProfileViewController: UIViewController {
         
     }
     
-    //MARK: - viewDidLoad
+    //MARK: - LifeCycle
+    // - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         print(self.editButton.frame) // (57.5, 587.0, 260.0, 50.0) На данном этапе выводит фрейм не загруженной кнопки - без применения констрейтов  (Как мы задали для iphone se) - В этом методе координаты не изменятся
     }
     
-    //MARK: - viewDidAppear
+    // - viewDidAppear
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print(self.editButton.frame) //(77.0, 782.0, 260.0, 50.0) - А тут выводит фрейм подогнанный под экран с помощью констрейтов (iphone 11) ... на разных экранах будут разные координаты
         setupDesign()
         print(self.view.frame.height)
-        print(self.avatarImageView.frame)
     }
     
     //MARK: - Methods
@@ -112,37 +114,54 @@ class ProfileViewController: UIViewController {
         self.descriptionLabel.lineBreakMode = .byWordWrapping
         //Setup Button
         self.editButton.layer.cornerRadius = 15
-        //Get short name from name
-        
+        //Check size screen
         switch self.view.frame.height {
-                      case 548.0...568.0:
-                       self.shortName.font = UIFont(name: "Roboto-regular", size: 100)//iPhone 5S,SE
-                      case 647.0...667.0:
-                       self.shortName.font = UIFont(name: "Roboto-regular", size: 115)//iPhone 6,7,8 SE2G
-                      case 716.0...736.0:
-                       self.shortName.font = UIFont(name: "Roboto-regular", size: 120)//iPhone 6+,7+,8+
-                      case 792...812.0:
-                       self.shortName.font = UIFont(name: "Roboto-regular", size: 130)//iPhone X,XS,XR
-                      case 876.0...926.0:
-                       self.shortName.font = UIFont(name: "Roboto-regular", size: 160)//iPhone XS_Max
-                      default: print("_____")
-                      }
+        
+            case 548.0...568.0://iPhone 5S,SE
+                self.shortName.font = UIFont(name: "Roboto-regular", size: 105)
+                self.descriptionLabel.font = UIFont(name: "SFProText-Regular", size: 12)
+                self.nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 21)
+                self.nameBetweenDesc.constant /= 2
+                self.nameBetweenImage.constant /= 2
+                self.imageBetweenTop.constant /= 2
+            case 647.0...667.0://iPhone 6,7,8 SE2G
+                self.shortName.font = UIFont(name: "Roboto-regular", size: 125)
+                self.descriptionLabel.font = UIFont(name: "SFProText-Regular", size: 14)
+                self.nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 23)
+                
+            case 716.0...736.0://iPhone 6+,7+,8+
+                self.shortName.font = UIFont(name: "Roboto-regular", size: 135)
+                self.descriptionLabel.font = UIFont(name: "SFProText-Regular", size: 16)
+                self.nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 24)
+                
+            case 792...812.0://iPhone X,XS,XR, 11
+                self.shortName.font = UIFont(name: "Roboto-regular", size: 140)
+                self.descriptionLabel.font = UIFont(name: "SFProText-Regular", size: 17)
+                self.nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 25)
+            case 897.0...926.0://iPhone XS_Max
+                self.descriptionLabel.font = UIFont(name: "SFProText-Regular", size: 18)
+                self.nameLabel.font = UIFont(name: "SFProDisplay-Bold", size: 27)
+                self.shortName.font = UIFont(name: "Roboto-regular", size: 145)
+            default: print("_____")
+        }
+        
+        //Get short name from name
         if let name = self.nameLabel.text{
             self.shortName.text = "\(name.split(separator: " ")[0].first ?? "n")\(name.split(separator: " ")[1].first ?? "n")".uppercased()
         }
-       
-
+        
+        
     }
     
     // Обработка тапа по картинке - вызываем actionSheet
     @objc func selectPhoto(_ sender: UITapGestureRecognizer){
-            
+        
         let alertSheet = UIAlertController(title: nil,
-                                        message: nil,
+                                           message: nil,
                                            preferredStyle: .actionSheet)
         let photo = UIAlertAction(title: "Выбрать фото", style: .default, handler: { _ in
             self.chooseImagePicker(source: .photoLibrary)
-                                    
+            
         })
         
         let camera = UIAlertAction(title: "Сделать фото", style: .default, handler: { _ in
@@ -157,7 +176,7 @@ class ProfileViewController: UIViewController {
         
     }
 }
-    
+
 //MARK: - Extension UIImagePicker - work with image
 extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -168,8 +187,8 @@ extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationCo
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
-             
-                
+            
+            
         }
     }
     
@@ -183,6 +202,6 @@ extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationCo
         self.avatarImageView.image = nil
         self.shortName.isHidden = false
         dismiss(animated: true)
-
+        
     }
 }
